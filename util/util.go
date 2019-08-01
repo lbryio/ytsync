6aca043f888c758b9217aef6fea4d35d39ae72e1
Mark Beamer Jr <markbeamerjr@gmail.com>
2019-07-31 22:42:22 -0400

    Add disabling of blob uploading for local testing until reflector docker image can be created( not urgent now ). Add ability to generate blocks when in regtest mode and using docker. Add refactor of environment variable accessing.

--- util/util.go
+++ util/util.go
@@ -6,12 +6,13 @@ import (
 	"os"
 	"os/user"
 
+	"github.com/lbryio/lbry.go/extras/errors"
+	"github.com/lbryio/lbry.go/lbrycrd"
+
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
-	"github.com/lbryio/lbry.go/extras/errors"
-
-	"github.com/prometheus/common/log"
+	log "github.com/sirupsen/logrus"
 )
 
 func GetBlobsDir() string {
@@ -19,13 +20,17 @@ func GetBlobsDir() string {
 	if blobsDir == "" {
 		usr, err := user.Current()
 		if err != nil {
-			log.Errorln(err.Error())
+			log.Error(err.Error())
 			return ""
 		}
 		blobsDir = usr.HomeDir + "/.lbrynet/blobfiles/"
 	}
 
 	return blobsDir
+}
+
+func IsBlobReflectionOff() bool {
+	return os.Getenv("REFLECT_BLOBS") == "false"
 }
 
 func GetLBRYNetDir() string {
@@ -41,13 +46,20 @@ func GetLBRYNetDir() string {
 	return lbrynetDir
 }
 
+const ALL = true
+const ONLINE = false
+
 func GetLBRYNetContainer(all bool) (*types.Container, error) {
+	return getDockerContainer("lbrynet", all)
+}
+
+func getDockerContainer(name string, all bool) (*types.Container, error) {
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
 	filters := filters.NewArgs()
-	filters.Add("name", "lbrynet")
+	filters.Add("name", name)
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: all, Filters: filters})
 	if err != nil {
 		panic(err)
@@ -59,7 +71,7 @@ func GetLBRYNetContainer(all bool) (*types.Container, error) {
 		return nil, nil
 	}
 	if len(containers) > 1 {
-		return nil, errors.Err("more than one lbrynet container found")
+		return nil, errors.Err("more than one %s container found", name)
 	}
 
 	return &containers[0], nil
@@ -67,4 +79,26 @@ func GetLBRYNetContainer(all bool) (*types.Container, error) {
 
 func IsUsingDocker() bool {
 	return os.Getenv("LBRYNET_USE_DOCKER") == "true"
+}
+
+func IsRegTest() bool {
+	return os.Getenv("REGTEST") == "true"
+}
+
+func GetLbrycrdClient(lbrycrdString string) (*lbrycrd.Client, error) {
+	var lbrycrdd *lbrycrd.Client
+	var err error
+	if lbrycrdString == "" {
+		lbrycrdd, err = lbrycrd.NewWithDefaultURL()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		lbrycrdd, err = lbrycrd.New(lbrycrdString)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return lbrycrdd, nil
 }
