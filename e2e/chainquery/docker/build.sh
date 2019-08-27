4548c410829553b7fc9da8e1bbf79c1006415562
Mark Beamer Jr <markbeamerjr@gmail.com>
2019-08-26 21:02:50 -0400

    Add chainquery to youtube sync repo

---
+++ e2e/chainquery/docker/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No docker tag argument supplied. Use './build.sh <tag>'"
+    exit 1
+fi
+docker build --build-arg VERSION=$1 --tag lbry/chainquery:$1 .
+docker push lbry/chainquery:$1
